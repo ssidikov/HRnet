d64d59cc513a55816fa3281cf4f7db56efc7c08b
Sardorbek SIDIKOV <sardorbek.sidikov7@gmail.com>
2024-12-27 20:47:15 +0100

    Refactor styles and update localStorage middleware to use action creators for better maintainability

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { employeesReducer } from './slices/employees-slice'
+import { addEmployee, setEmployees } from './slices/employees-slice'
 
 // Middleware to maintain a state in Localstorage
 const localStorageMiddleware = (store) => (next) => (action) => {
   const result = next(action)
-  if (['employees/addEmployee', 'employees/setEmployees'].includes(action.type)) {
+  const actionTypes = [addEmployee.type, setEmployees.type]
+  if (actionTypes.includes(action.type)) {
     const { employees } = store.getState().employees
     localStorage.setItem('employees', JSON.stringify(employees))
   }
